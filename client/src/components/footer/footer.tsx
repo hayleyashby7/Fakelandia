a7bc00c825458ec6caa5126edd24ff112ea48e32
Hayley Ashby <hayleyashby7@gmail.com>
2023-07-29 13:31:05 +0100

    feat: :sparkles: Footer component added with styling changes for layout

---
+++ client/src/components/footer/footer.tsx
@@ -0,0 +1,7 @@
+const Footer = () => (
+	<footer>
+		<p>@ Hayley Ashby 2023</p>
+	</footer>
+);
+
+export default Footer;
--- client/src/components/layouts/main_layout.tsx
+++ client/src/components/layouts/main_layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 
 export const MainLayout = () => (
-	<>
+	<div className='main'>
 		<Header />
-		<main className='p-6' style={{ flex: '1' }}>
+		<main className='p-6 main__content'>
 			<Outlet />
 		</main>
-	</>
+		<Footer />
+	</div>
 );
