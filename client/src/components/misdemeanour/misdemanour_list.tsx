5cff93f4a88c80b3b9c6bf83f9fea35a66ee442f
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-07 20:02:26 +0100

    feat: :sparkles: Misdemeanour table concatenates crime and emoji

--- client/src/components/misdemeanour/misdemanour_list.tsx
+++ client/src/components/misdemeanour/misdemanour_list.tsx
@@ -1,4 +1,4 @@
-import { Misdemeanour } from '../../types/misdemeanours.types';
+import { MISDEMEANOUR_EMOJIS, Misdemeanour, MisdemeanourAndEmoji } from '../../types/misdemeanours.types';
 import Table from '../table/table';
 
 interface Misdemeanour_ListProps {
@@ -6,10 +6,17 @@ interface Misdemeanour_ListProps {
 }
 
 export const Misdemeanour_List: React.FC<Misdemeanour_ListProps> = ({ data }) => {
+	const addEmoji = (misdemeanour: Misdemeanour): Misdemeanour => {
+		return {
+			citizenId: misdemeanour.citizenId,
+			date: misdemeanour.date,
+			misdemeanour: `${misdemeanour.misdemeanour} ${MISDEMEANOUR_EMOJIS[misdemeanour.misdemeanour]}` as MisdemeanourAndEmoji,
+		};
+	};
+
 	if (data) {
 		const misdemeanours = Object.entries(data);
-		const list = Object.entries(misdemeanours[0][1]).map(([, item]) => item as Misdemeanour);
-		
+		const list = Object.entries(misdemeanours[0][1]).map(([, item]) => addEmoji(item as Misdemeanour));
 		return <Table data={list} />;
 	} else {
 		return (
--- client/src/types/misdemeanours.types.ts
+++ client/src/types/misdemeanours.types.ts
@@ -1,16 +1,22 @@
-export const MISDEMEANOURS = [
-	'rudeness',
-	'vegetables',
-	'lift',
-	'united',
-] as const;
+export const MISDEMEANOURS = ['rudeness', 'vegetables', 'lift', 'united'] as const;
 export type MisdemeanourKind = (typeof MISDEMEANOURS)[number];
 
 export const JUST_TALK = 'just-talk';
 export type JustTalk = typeof JUST_TALK;
 
+export enum MISDEMEANOUR_EMOJIS {
+	rudeness = '🤪',
+	vegetables = '🗣',
+	lift = '🥗',
+	united = '😈',
+}
+
+export type MisdemeanourEmoji = (typeof MISDEMEANOUR_EMOJIS)[MisdemeanourKind];
+
+export type MisdemeanourAndEmoji = MisdemeanourKind & MisdemeanourEmoji;
+
 export type Misdemeanour = {
 	citizenId: number;
-	misdemeanour: MisdemeanourKind;
+	misdemeanour: MisdemeanourKind | MisdemeanourAndEmoji;
 	date: string; // we'll stringify this for easy sending via HTTP rather than storing the full Date object
 };
