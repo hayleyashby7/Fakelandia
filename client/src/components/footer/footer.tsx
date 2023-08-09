f0116f5fde946135e7814e4b026c1c302e1e101a
Hayley Ashby <hayleyashby7@gmail.com>
2023-08-09 16:28:47 +0100

    style: :lipstick: Sticky footer

--- client/src/components/footer/footer.tsx
+++ client/src/components/footer/footer.tsx
@@ -1,5 +1,5 @@
-const Footer: React.FC  = () => (
-	<footer>
+const Footer: React.FC = () => (
+	<footer className='fixed left-0 bottom-0 text-center w-screen bg-red text-gold text-lg'>
 		<p>@ Hayley Ashby 2023</p>
 	</footer>
 );
